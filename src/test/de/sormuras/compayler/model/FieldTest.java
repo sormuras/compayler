1fab709ba46b76ad26561b6266ddd8d6f8c843a4
sormuras <sormuras@gmail.com>
2014-01-25 21:39:49 +0100

    Field test added.

---
+++ src/test/de/sormuras/compayler/model/FieldTest.java
@@ -0,0 +1,14 @@
+package de.sormuras.compayler.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FieldTest {
+
+  @Test
+  public void testBrackets() {
+    Field field = new Field();
+    Assert.assertNotNull(field);
+  }
+
+}
--- src/test/de/sormuras/compayler/model/TypeTest.java
+++ src/test/de/sormuras/compayler/model/TypeTest.java
@@ -1,36 +1,37 @@
 package de.sormuras.compayler.model;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.HashMap;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class TypeTest {
 
   @Test
   public void testBrackets() {
-    Assert.assertEquals("", Type.brackets(0, true));
-    Assert.assertEquals("", Type.brackets(0, false));
-    Assert.assertEquals("...", Type.brackets(1, true));
-    Assert.assertEquals("[]", Type.brackets(1, false));
-    Assert.assertEquals("[]...", Type.brackets(2, true));
-    Assert.assertEquals("[][]", Type.brackets(2, false));
-    Assert.assertEquals("[][]...", Type.brackets(3, true));
-    Assert.assertEquals("[][][]", Type.brackets(3, false));
-    Assert.assertEquals("[][][]...", Type.brackets(4, true));
-    Assert.assertEquals("[][][][]", Type.brackets(4, false));
-    Assert.assertEquals("[][][][]...", Type.brackets(5, true));
-    Assert.assertEquals("[][][][][]", Type.brackets(5, false));
+    assertEquals("", Type.brackets(0, true));
+    assertEquals("", Type.brackets(0, false));
+    assertEquals("...", Type.brackets(1, true));
+    assertEquals("[]", Type.brackets(1, false));
+    assertEquals("[]...", Type.brackets(2, true));
+    assertEquals("[][]", Type.brackets(2, false));
+    assertEquals("[][]...", Type.brackets(3, true));
+    assertEquals("[][][]", Type.brackets(3, false));
+    assertEquals("[][][]...", Type.brackets(4, true));
+    assertEquals("[][][][]", Type.brackets(4, false));
+    assertEquals("[][][][]...", Type.brackets(5, true));
+    assertEquals("[][][][][]", Type.brackets(5, false));
   }
 
   @Test
   public void testToString() {
     int[][][] m3 = new int[1][1][1];
-    Assert.assertEquals("int[][][]", Type.forName(m3.getClass().getCanonicalName()).toString());
-    Assert.assertEquals("java.lang.Integer", Type.forName(new Integer(3).getClass().getCanonicalName()).toString());
-    Assert.assertEquals("java.util.HashMap", Type.forName(new HashMap<String, String>().getClass().getCanonicalName()).toString());
-    Assert.assertEquals("java.util.HashMap<String, String>", Type.forName("java.util.HashMap", "<String, String>").toString());
-    Assert.assertEquals("java.util.HashMap[][]", Type.forName("java.util.HashMap", 2).toString());
+    assertEquals("int[][][]", Type.forName(m3.getClass().getCanonicalName()).toString());
+    assertEquals("java.lang.Integer", Type.forName(new Integer(3).getClass().getCanonicalName()).toString());
+    assertEquals("java.util.HashMap", Type.forName(new HashMap<String, String>().getClass().getCanonicalName()).toString());
+    assertEquals("java.util.HashMap<String, String>", Type.forName("java.util.HashMap", "<String, String>").toString());
+    assertEquals("java.util.HashMap[][]", Type.forName("java.util.HashMap", 2).toString());
   }
 
 }
