7f7150987b5390c4bb3aecd80ee0cc8de54ea8dd
sormuras <sormuras@gmail.com>
2014-01-26 20:53:41 +0100

    Refactoring in progress.

--- src/main/de/sormuras/compayler/service/impl/DefaultSignatureFactory.java
+++ src/main/de/sormuras/compayler/service/impl/DefaultSignatureFactory.java
@@ -1,18 +1,92 @@
 package de.sormuras.compayler.service.impl;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import de.sormuras.compayler.Compayler;
+import de.sormuras.compayler.ExecutionTime;
+import de.sormuras.compayler.model.Field;
 import de.sormuras.compayler.model.Signature;
+import de.sormuras.compayler.model.Type;
 import de.sormuras.compayler.service.SignatureFactory;
 
 public class DefaultSignatureFactory implements SignatureFactory<Method> {
 
+  public static boolean isAnnotationPresent(Class<? extends Annotation> annotationClass, Annotation... annotations) {
+    for (Annotation annotation : annotations)
+      if (annotation.annotationType() == annotationClass)
+        return true;
+    return false;
+  }
+
+  public static boolean isExecutionTimePresent(Annotation... annotations) {
+    return isAnnotationPresent(ExecutionTime.class, annotations);
+  }
+
+  public static Map<String, Boolean> buildNameIsUniqueMap(Class<?> interfaceClass) {
+    Map<String, Boolean> uniques = new HashMap<>();
+    for (Method method : interfaceClass.getMethods()) {
+      String name = method.getName();
+      Boolean old = uniques.put(name, Boolean.TRUE);
+      if (old == null)
+        continue;
+      uniques.put(name, Boolean.FALSE);
+    }
+    return uniques;
+  }
+
+  public static int dimension(Class<?> classType) {
+    int dimension = 0;
+    while (classType.isArray()) {
+      classType = classType.getComponentType();
+      dimension++;
+    }
+    return dimension;
+  }
+
+  public static Type buildType(Class<?> classType) {
+    return Type.forName(classType.getCanonicalName(), dimension(classType));
+  }
+
   @Override
   public List<Signature<Method>> createSignatures(Compayler compayler) {
     List<Signature<Method>> signatures = new LinkedList<>();
+
+    Class<?> interfaceClass = compayler.getInterfaceClass();
+    if (interfaceClass == null)
+      throw new RuntimeException("Can't create signatures for " + compayler.getInterfaceClassName());
+
+    Map<String, Boolean> uniques = buildNameIsUniqueMap(interfaceClass);
+    for (Method method : interfaceClass.getMethods()) {
+      // simple strings
+      String name = method.getName();
+      Type returnType = buildType(method.getReturnType());
+      // collect exception type names
+      List<Type> throwables = new ArrayList<>();
+      for (Class<?> exceptionType : method.getExceptionTypes())
+        throwables.add(buildType(exceptionType));
+      // parse parameters to fields
+      List<Field> fields = new ArrayList<>();
+      int lastIndex = method.getParameterTypes().length - 1;
+      for (int index = 0; index <= lastIndex; index++) {
+        Field field = new Field();
+        field.setIndex(index);
+        field.setName("p" + index);
+        field.setTime(isExecutionTimePresent(method.getParameterAnnotations()[index]));
+        field.setType(buildType(method.getParameterTypes()[index]));
+        field.setVariable(index == lastIndex && method.isVarArgs());
+        fields.add(field);
+      }
+      // create signature
+      Signature<Method> signature = new Signature<>(method, name, returnType, fields, throwables, uniques.get(name));
+      signatures.add(signature);
+    }
+
     return signatures;
   }
 
--- src/main/de/sormuras/compayler/service/impl/DefaultSourceFactory.java
+++ src/main/de/sormuras/compayler/service/impl/DefaultSourceFactory.java
@@ -1,6 +1,10 @@
 package de.sormuras.compayler.service.impl;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import de.sormuras.compayler.Compayler;
 import de.sormuras.compayler.Source;
@@ -8,11 +12,21 @@ import de.sormuras.compayler.model.Description;
 import de.sormuras.compayler.service.SourceFactory;
 
 public class DefaultSourceFactory implements SourceFactory {
+  
+  public static String now() {
+    TimeZone tz = TimeZone.getTimeZone("UTC");
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mmZ");
+    df.setTimeZone(tz);
+    return df.format(new Date());
+  } 
 
   @Override
   public <X> Source createSource(Compayler compayler, List<Description<X>> descriptions) {
     Lines lines = new Lines();
-    lines.add("/**", " * Generated for " + compayler, " */");
+    lines.add("/**", " * Class " + compayler.getDecoratorClassName() + " generated for " + compayler.getInterfaceName() + ".", " */");
+    for (Description<X> description : descriptions) {
+      lines.add("// " + description.getSignature().getName());
+    }
     return new Source(compayler.getDecoratorPackage(), compayler.getDecoratorName(), lines.getLines());
   }
 
