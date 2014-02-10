16745f3ef5a5127571b6cd4e76ebb0423ee129eb
sormuras <sormuras@gmail.com>
2014-02-10 19:20:00 +0100

    Let source test class implement Callable<Long>

--- src/test/org/prevayler/contrib/compayler/SourceTest.java
+++ src/test/org/prevayler/contrib/compayler/SourceTest.java
@@ -1,9 +1,8 @@
 package org.prevayler.contrib.compayler;
 
-import static java.lang.String.format;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,15 +13,17 @@ public class SourceTest {
   public void testCompilation() throws Exception {
     long expected = 1701L;
     List<String> code = new ArrayList<>();
-    code.add("public class Test {");
-    code.add("  public long test() {");
-    code.add(format("    return %d;", expected));
+    code.add("public class Test implements java.util.concurrent.Callable<Long> {");
+    code.add("  @Override");
+    code.add("  public Long call() {");
+    code.add("    return Long.valueOf(" + expected + ");");
     code.add("  }");
     code.add("}");
     Source source = new Source("", "Test", code);
     ClassLoader loader = source.compile();
-    Object test = loader.loadClass("Test").newInstance();
-    Assert.assertEquals(expected, (long) test.getClass().getDeclaredMethod("test").invoke(test));
+    @SuppressWarnings("unchecked")
+    Callable<Long> test = (Callable<Long>) loader.loadClass("Test").newInstance();
+    Assert.assertEquals(expected, test.call().longValue());
   }
 
 }
