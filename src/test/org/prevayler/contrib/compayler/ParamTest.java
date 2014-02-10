5f7ce926c05bac32d91ed2aa7ad87ab9132ad1b2
sormuras <sormuras@gmail.com>
2014-02-10 10:38:58 +0100

    Added merged parameters test.

--- src/test/org/prevayler/contrib/compayler/ParamTest.java
+++ src/test/org/prevayler/contrib/compayler/ParamTest.java
@@ -1,11 +1,24 @@
 package org.prevayler.contrib.compayler;
 
 import static org.junit.Assert.assertEquals;
+import static org.prevayler.contrib.compayler.Util.merge;
 
 import org.junit.Test;
 
 public class ParamTest {
 
+  @Test
+  public void testMerged() {
+    Param oracle = new Param();
+    oracle.setName("oracle");
+    oracle.setType(new Type(Boolean.class));
+    Param integers = new Param();
+    integers.setName("integers");
+    integers.setType(new Type(int[].class));
+    integers.setVariable(true);
+    assertEquals("(java.lang.Boolean oracle, int... integers)", merge("(", ")", ", ", oracle, integers));
+  }
+  
   @Test
   public void testToString() {
     Param param = new Param();
